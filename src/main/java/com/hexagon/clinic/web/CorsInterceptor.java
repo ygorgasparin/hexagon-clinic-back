c33aeba0ec857c88dbbf502b9712858c72e5163f
ygor.gasparin <ygor.gasparin@gmail.com>
2017-07-12 12:07:35 -0300

    Cors

--- src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
+++ src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
@@ -11,14 +11,13 @@ public class CorsInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (!request.getMethod().equalsIgnoreCase("OPTIONS"))
+            response.setHeader("Access-Control-Allow-Origin", "*");
+
 //        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept,X-Requested-With");
 //        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE");
-//
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
         return super.preHandle(request, response, handler);
     }
 
