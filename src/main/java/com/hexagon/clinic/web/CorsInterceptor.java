d9939a4bba023e9a8dbcf4bd93464c78d68efd14
ygor.gasparin <ygor.gasparin@gmail.com>
2017-07-12 11:54:11 -0300

    Cors

--- src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
+++ src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
@@ -1,5 +1,6 @@
 package com.hexagon.clinic.web;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,15 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CorsInterceptor extends HandlerInterceptorAdapter {
 
-    private static final String ORIGIN_NAME = "Access-Control-Allow-Origin";
-    private static final String METHODS_NAME = "Access-Control-Allow-Methods";
-    private static final String HEADERS_NAME = "Access-Control-Allow-Headers";
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        response.setHeader(ORIGIN_NAME, "*");
-        response.setHeader(HEADERS_NAME, "Content-Type, Content-Length, Authorization, Accept,X-Requested-With");
-        response.setHeader(METHODS_NAME, "GET, OPTIONS, POST, PUT, DELETE");
-        return true;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept,X-Requested-With");
+//        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE");
+//
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
     }
 }
