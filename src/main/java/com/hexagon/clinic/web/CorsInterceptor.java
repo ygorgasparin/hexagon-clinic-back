133cc703f555ea26424c8963699b5d5767a4746f
ygor.gasparin <ygor.gasparin@gmail.com>
2017-07-12 12:43:03 -0300

    Access-Control-Allow-Headers

--- src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
+++ src/main/java/com/hexagon/clinic/web/CorsInterceptor.java
@@ -16,7 +16,7 @@ public class CorsInterceptor extends HandlerInterceptorAdapter {
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+            response.setHeader("Access-Control-Allow-Headers", "*");
         }
 
         return super.preHandle(request, response, handler);
